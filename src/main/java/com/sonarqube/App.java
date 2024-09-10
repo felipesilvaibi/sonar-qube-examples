9beaf60fdf69116d32ded64a02c7a23d1913d750
Darkeide <rafaeltambani01@gmail.com>
2024-09-10 18:07:23 -0300

    testando

--- src/main/java/com/sonarqube/App.java
+++ src/main/java/com/sonarqube/App.java
@@ -9,6 +9,18 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
+        
+
+    
+
+            System.out.print("Digite a temperatura em graus Celsius: ");
+            double celsius = 100;
+
+            double fahrenheit = (celsius * 9/5) + 32;
+    
+            // Exibe a temperatura em Fahrenheit
+            System.out.printf("A temperatura em Fahrenheit criando a mesma conta é: %.2f%n", (celsius * 9/5) + 32);
+
+            System.out.println("A temperatura em Fahrenheit usando o calculo é: "+fahrenheit);
     }
 }
