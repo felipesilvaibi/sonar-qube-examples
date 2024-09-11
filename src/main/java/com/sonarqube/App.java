27b69118420560f9251bab69bd96193b84339a40
Burunu <brunojv15@gmail.com>
2024-09-11 11:28:05 -0300

    feat: 'Random' objects should be reused

--- src/main/java/com/sonarqube/App.java
+++ src/main/java/com/sonarqube/App.java
@@ -11,7 +11,7 @@ public class App
     public static void main( String[] args ) 
     {
         Estacionamento estacionamento = new Estacionamento();
-        estacionamento.assignSpot();
+        estacionamento.Vaga();
     }
 
     public class Estacionamento {
