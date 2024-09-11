2c4a386421ff7ba64adddd1096ca97c11ba2a54c
jean.rothenburg <jeancarlosrothenburg13@gmail.com>
2024-09-11 00:13:14 -0300

    feat: 'if ..  else if' constructs should end with 'else' clauses

--- src/main/java/com/sonarqube/App.java
+++ src/main/java/com/sonarqube/App.java
@@ -3,13 +3,14 @@ package com.sonarqube;
 public class App {
 
     public static void main( String[] args ) {
-        int x;
+        int x = 0;
 
-        if (x == 0){
+        if (x < 0){
 
-        }else if(x == 1){
-            
+        }else if(x == 0){
+
         }
+        
     }
 
 }
