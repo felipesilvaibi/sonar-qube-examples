d1458b4f9fde602ab31385c01d7e2bd257a63e84
jean.rothenburg <jeancarlosrothenburg13@gmail.com>
2024-09-10 22:44:34 -0300

    feat: geração intencional de erro por uso de print

--- src/main/java/com/sonarqube/App.java
+++ src/main/java/com/sonarqube/App.java
@@ -1,14 +1,9 @@
 package com.sonarqube;
 
-/**
- * Hello world!!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
+
 }
