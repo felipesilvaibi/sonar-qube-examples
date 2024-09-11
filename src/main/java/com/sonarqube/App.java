8499dece3ca8757a3eaf883dc5488c8e351486de
Burunu <brunojv15@gmail.com>
2024-09-11 11:08:24 -0300

    feat: 'Random' objects should be reused

--- src/main/java/com/sonarqube/App.java
+++ src/main/java/com/sonarqube/App.java
@@ -1,5 +1,6 @@
 package com.sonarqube;
-import java.util.concurrent.atomic.AtomicInteger; 
+
+import java.util.Random;
 
 /**
  * Hello world!!
@@ -9,14 +10,15 @@ public class App
 {
     public static void main( String[] args ) 
     {
-        AtomicInteger a = new AtomicInteger(5);
-        AtomicInteger b = new AtomicInteger(5);
-
-        // Comparação incorreta usando equals()
-        if (a.equals(b)) {
-            System.out.println("A e B são iguais (equals)");  // Não será impresso, pois equals() verifica referência.
-        } else {
-            System.out.println("A e B NÃO são iguais (equals)");
-        }
+        Estacionamento estacionamento = new Estacionamento();
+        estacionamento.assignSpot();
     }
+
+    class Estacionamento {
+    public void Vaga() {
+        Random random = new Random();  
+        int vagaNumero = random.nextInt(100); 
+        System.out.println("Vaga Numero: " + vagaNumero);
+    }
+}
 }
