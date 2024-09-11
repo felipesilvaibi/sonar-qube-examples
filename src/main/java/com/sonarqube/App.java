94e40e1bf7069d355192d8b5425f5d94aeeb8110
root <root@Lipe>
2024-09-10 23:32:39 -0300

    feat: switch case without default

--- src/main/java/com/sonarqube/App.java
+++ src/main/java/com/sonarqube/App.java
@@ -1,4 +1,5 @@
 package com.sonarqube;
+import java.util.logging.Logger;
 
 /**
  * Hello world!!
@@ -6,8 +7,34 @@ package com.sonarqube;
  */
 public class App 
 {
+    private static final Logger LOGGER = Logger.getLogger(App.class.getName());
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        test("^");
+        test("v");
+        test(">");
+        test("<");
+        // App.test("x"); quebra!
+    }
+
+    public static void test(String param) {
+        switch (param) {
+        case "^":
+            LOGGER.info("cima");
+            break;
+        case "v":
+            LOGGER.info("baixo");
+            break;
+        case ">":
+            LOGGER.info("direita");
+            break;
+        case "<":
+            LOGGER.info("esquerda");
+            break;
+        // default:
+        //     LOGGER.warning("comando inválido");
+        //     break;
+        }
     }
 }
