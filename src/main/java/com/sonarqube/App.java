4c13d13954c9c6e099d7ff5d3a5c01185d4a5085
root <root@Lipe>
2024-09-11 00:24:17 -0300

    feat: should breaks on division by zero

--- src/main/java/com/sonarqube/App.java
+++ src/main/java/com/sonarqube/App.java
@@ -5,36 +5,19 @@ import java.util.logging.Logger;
  * Hello world!!
  *
  */
-public class App 
-{
+public class App {
     private static final Logger LOGGER = Logger.getLogger(App.class.getName());
 
-    public static void main( String[] args )
-    {
-        test("^");
-        test("v");
-        test(">");
-        test("<");
-        // App.test("x"); quebra!
+    public static void main(String[] args) {
+        int result;
+
+        result = test(10, 5);
+        LOGGER.info("Resultado: " + result);
+
+        // result = test(10, 0); quebra!
     }
 
-    public static void test(String param) {
-        switch (param) {
-        case "^":
-            LOGGER.info("cima");
-            break;
-        case "v":
-            LOGGER.info("baixo");
-            break;
-        case ">":
-            LOGGER.info("direita");
-            break;
-        case "<":
-            LOGGER.info("esquerda");
-            break;
-        // default:
-        //     LOGGER.warning("comando inválido");
-        //     break;
-        }
+    public static int test(int dividendo, int divisor) {
+        return dividendo / divisor;
     }
 }
